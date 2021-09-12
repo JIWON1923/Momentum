1d0505466e1944ef0a8d7f01e66f4f846820ae0a
jiwon <zest1923@gmail.com>
2021-09-12 21:48:06 +0900

    Calculator using return keyword

--- Script.js
+++ Script.js
@@ -1,13 +1,18 @@
 const Calculator = {
-    plus: function(a, b){ console.log(a+b); },
-    minus: function (a,b) { console.log (a-b); },
-    multi: function (a,b) { console.log (a*b); },
-    divide: function (a,b) { console.log (a/b); },
-    power: function (a,b) { console.log (a**b); },
+    plus: function(a, b){ return a+b; },
+    minus: function (a,b) { return a-b; },
+    multi: function (a,b) { return a*b; },
+    divide: function (a,b) { return a/b; },
+    power: function (a,b) { return a**b; },
 };
 
-Calculator.plus(10,5)
-Calculator.multi(10,5)
-Calculator.minus(10,5)
-Calculator.divide(10,5)
-Calculator.power(10,5)
+result = Calculator.plus(10,5);
+console.log(result);
+result = Calculator.multi(10,5);
+console.log(result);
+result = Calculator.minus(10,5);
+console.log(result);
+result = Calculator.divide(10,5);
+console.log(result);
+result = Calculator.power(10,5);
+console.log(result);
