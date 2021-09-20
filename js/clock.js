7258ff27cc74b64b61e0b00fdef391845393852c
jiwon <zest1923@gmail.com>
2021-09-20 22:05:36 +0900

    basic clock

--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,9 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello(){
-    console.log("Hello");
+function getClock(){
+    const date = new Date();
+    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
-//setInterval(sayHello, 5000);
-setTimeout(sayHello, 5000);
+getClock(); // interval을 실행하기까지 1초를 기다려야하기 때문에 우선 1개 실행
+setInterval(getClock, 1000); // 매 초마다 Date Object 생성
